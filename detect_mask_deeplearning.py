08a992fa50b20f5058fbb9e1f310f3e82319e6d7
sudhankandel <sudhankandel03@gmail.com>
2020-08-12 19:38:32 +0545

    bug fixed

--- detect_mask_deeplearning.py
+++ detect_mask_deeplearning.py
@@ -26,69 +26,56 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	preds = []
 
 	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
+	
 		confidence = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
+	
 		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
+
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
+
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
+	
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (100, 100))
 			face = img_to_array(face)
 			
 
-			# add the face and bounding boxes to their respective
-			# lists
+	
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
+
 	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
+	
 		faces = np.array(faces, dtype="float32")
 		preds = maskNet.predict(faces, batch_size=32)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
+
 	return (locs, preds)
 
-# load our serialized face detector model from disk
+
 prototxtPath = r"./face_detector/deploy.prototxt"
 weightsPath = r"./face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
+
 maskNet = load_model("mask_detector.model")
 
-# initialize the video stream
+
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 
-# loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 	for (box, pred) in zip(locs, preds):
 		(x,y,x2,y2)=box
